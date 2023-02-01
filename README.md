e3ecf371252881aea99d0f86141aeaaf04239f1d
yhuj79 <yu1085@naver.com>
2023-02-01 09:17:07 +0900

    기타 버그 수정 / 스타일 수정 / README 변경

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-### Simple JavaScript Web Game
+<img src="https://raw.githubusercontent.com/yhuj79/BounceBall-Game/master/image/thumbnail.png">
 
-<img src=https://user-images.githubusercontent.com/96750543/149974716-48cfd68a-1d9e-4db8-a304-3a10fa7a15ed.png>
+Javascript로 제작한 공 튀기기 미니 게임
 
+MDN에서 제공하는 튜토리얼 <a target="_blank" rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript">2D breakout game using pure JavaScript</a>을 응용하여 제작
 #### :ballot_box_with_check: <a target="_blank" rel="noopener noreferrer" href="https://yhuj79.github.io/BounceBall-Game/">GitHub Page Link</a>
+
+## Built With
+
+<img src="https://skillicons.dev/icons?i=html,css,js" alt="techstack" /> 
+
+## About The Project
+
+### 게임 정보
+
+    공을 벽에 튀기면 점수를 획득
+    공이 떨어지면 게임 오버
+    시간이 지남에 따라 게임 패턴에 변화가 일어남
+
+## 참조
+
+[https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript](https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript)
--- brick.js
+++ brick.js
@@ -7,14 +7,14 @@ var dx = 4;
 var dy = 4;
 var ballRadius = 16;
 var score = 0;
-var ballcolor = "firebrick";
+var ballColor = "firebrick";
 var paddleColor = "royalblue";
-var paddleWidth = 150;
+var paddleWidth = 200;
 var paddleHeight = 20;
-var paddleX = (canvas.width - paddleWidth) / 2;
+var paddleX = (canvas.width - paddleWidth) * 2;
 var leftPressed = false;
 var rightPressed = false;
-var scoreColor = "black";
+var scoreColor = "#000";
 
 var hitAudio1 = new Audio("./sound/hitSound.mp3");
 var hitAudio2 = new Audio("./sound/hitSound.mp3");
@@ -65,7 +65,7 @@ function keyUpHandler(e) {
 }
 function mouseMoveHandler(e) {
   var relativeX = e.clientX - canvas.offsetLeft;
-  if (relativeX > 0 && relativeX < canvas.width) {
+  if (relativeX > 50 && relativeX < canvas.width - 50) {
     paddleX = relativeX - paddleWidth / 2;
   }
 }
@@ -73,7 +73,7 @@ function mouseMoveHandler(e) {
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-  ctx.fillStyle = ballcolor;
+  ctx.fillStyle = ballColor;
   ctx.fill();
   ctx.closePath();
 }
@@ -92,23 +92,34 @@ function drawScore() {
   ctx.fillText("SCORE : " + score, 8, 35);
 }
 
+function randomNum(min, max) {
+  var randNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randNum;
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   drawPaddle();
   drawScore();
 
-  if (x > canvas.width - ballRadius || x < ballRadius) {
+  if (x > canvas.width - ballRadius || x + 10 < ballRadius) {
+    let num = randomNum(0, 3);
     dx = -dx;
+    dx + num;
     score++;
     hitAudio1.play();
-  } else if (y < ballRadius) {
+  } else if (y > canvas.width - ballRadius || y + 10 < ballRadius) {
+    let num = randomNum(0, 3);
     dy = -dy;
+    dy + num;
     score++;
     hitAudio2.play();
   } else if (y > canvas.height - ballRadius - paddleHeight) {
     if (x > paddleX && x < paddleX + paddleWidth) {
+      let num = randomNum(0, 3);
       dy = -dy;
+      dy + num;
       hitAudio2.play();
     } else if (y > canvas.height) {
       hitAudio1 = new Audio("./sound/null.mp3");
@@ -118,45 +129,13 @@ function draw() {
       gameMsg.classList.add(SHOW);
       dx = 0;
       dy = 0;
-      scoreColor = "#eee";
+      scoreColor = "#EEE";
       endAudio = new Audio("./sound/null.mp3");
       canvas.width = 0;
       restartGame.innerHTML = "RELOAD";
       restartGame.classList.add(SHOW);
     }
   }
-
-  //game pattern
-  setInterval(function () {
-    ballRadius = 12;
-  }, 15000);
-  setInterval(function () {
-    paddleColor = "darkblue";
-  }, 25000);
-  setInterval(function () {
-    paddleWidth = 75;
-  }, 30000);
-  setInterval(function () {
-    ballcolor = "purple";
-  }, 55000);
-  setInterval(function () {
-    ballcolor = "#eee";
-  }, 60000);
-  setInterval(function () {
-    ballcolor = "#eee";
-  }, 60100);
-  setInterval(function () {
-    ballcolor = "purple";
-  }, 60200);
-  setInterval(function () {
-    ballcolor = "#eee";
-  }, 60400);
-  setInterval(function () {
-    ballcolor = "#eee";
-  }, 60500);
-  setInterval(function () {
-    ballcolor = "purple";
-  }, 60600);
 
   if (leftPressed && paddleX > 0) {
     paddleX -= 6;
@@ -166,4 +145,118 @@ function draw() {
 
   x += dx;
   y += dy;
+
+  //game pattern
+
+  setInterval(function () {
+    ballRadius = 12;
+  }, 30000);
+  setInterval(function () {
+    paddleColor = "darkblue";
+  }, 55000);
+  setInterval(function () {
+    paddleWidth = 80;
+  }, 60000);
+  setInterval(function () {
+    ballColor = "purple";
+  }, 95000);
+  setInterval(function () {
+    ballColor = "#eee";
+  }, 100000);
+  setInterval(function () {
+    ballColor = "#eee";
+  }, 100100);
+  setInterval(function () {
+    ballColor = "#eee";
+  }, 100200);
+  setInterval(function () {
+    ballColor = "#eee";
+  }, 100300);
+  setInterval(function () {
+    ballColor = "#eee";
+  }, 100400);
+  setInterval(function () {
+    ballColor = "#eee";
+  }, 100500);
+  setInterval(function () {
+    ballColor = "#eee";
+  }, 100600);
+  setInterval(function () {
+    ballColor = "#eee";
+  }, 100700);
+  setInterval(function () {
+    ballColor = "purple";
+  }, 100800);
+  setInterval(function () {
+    ballColor = "#eee";
+  }, 100900);
+  setInterval(function () {
+    ballcolor = "#eee";
+  }, 101000);
+  setInterval(function () {
+    ballColor = "#eee";
+  }, 101100);
+  setInterval(function () {
+    ballcolor = "#eee";
+  }, 101200);
+  setInterval(function () {
+    ballColor = "#eee";
+  }, 101300);
+  setInterval(function () {
+    ballcolor = "#eee";
+  }, 101400);
+  setInterval(function () {
+    ballColor = "#eee";
+  }, 101500);
+  setInterval(function () {
+    ballcolor = "#eee";
+  }, 101600);
+  setInterval(function () {
+    ballColor = "purple";
+  }, 101700);
+  setInterval(function () {
+    ballRadius = 8;
+  }, 160000);
+  setInterval(function () {
+    ballRadius = 8;
+  }, 160100);
+  setInterval(function () {
+    ballRadius = 8;
+  }, 160200);
+  setInterval(function () {
+    paddleWidth = 40;
+  }, 220000);
+  setInterval(function () {
+    paddleWidth = 40;
+  }, 220100);
+  setInterval(function () {
+    paddleWidth = 40;
+  }, 220200);
+  setInterval(function () {
+    paddleWidth = 40;
+  }, 220300);
+  setInterval(function () {
+    paddleWidth = 40;
+  }, 220400);
+  setInterval(function () {
+    paddleWidth = 40;
+  }, 220500);
+  setInterval(function () {
+    paddleWidth = 40;
+  }, 220600);
+  setInterval(function () {
+    paddleWidth = 40;
+  }, 220700);
+  setInterval(function () {
+    paddleWidth = 40;
+  }, 220800);
+  setInterval(function () {
+    paddleWidth = 40;
+  }, 220900);
+  setInterval(function () {
+    paddleWidth = 40;
+  }, 221000);
+  setInterval(function () {
+    paddleWidth = 40;
+  }, 221100);
 }
